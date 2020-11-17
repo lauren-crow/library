cf924c4a0398cc55af58de205e0e46e03a9d7f0c
Lauren Crow <hi@lcrow.studio>
2020-11-17 17:54:44 +0100

    Basic styling

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
+//add padding around all content
+//'add' button brings up window with inputs - after submit, window collapses and button reappears
+//should books be in list or be tiles (tile?)
+//login?
+
 let titleInput = document.querySelector('#titleInput');
 let authorInput = document.querySelector('#authorInput');
 let pagesInput = document.querySelector('#pagesInput');
 let readInput = document.querySelector('#readInput');
-let bookButton = document.querySelector('#bookButton');
+let submitBrn = document.querySelector('#submitBtn');
 let bookList = document.querySelector('.bookList');
+let addBookBtn = document.querySelector('#addBookBtn');
+let form = document.querySelector('.form');
 
 let myLibrary = [
-    {
+    /*{
         title: "The Hobbit",
         author: "Tolkien",
         pages: "102",
         read: "yes"
-      },
+      },*/
 ];
+
+const printNewBook = function(){
+    const bookTile = document.createElement('div');
+    bookTile.className = ("bookTile");
+    bookList.appendChild(bookTile);
+
+    const tileTitle = document.createElement('div');
+    tileTitle.className = ("tileTitle");
+    bookTile.appendChild(tileTitle);
+    tileTitle.innerHTML = myLibrary[myLibrary.length - 1].title;
+
+    const tileAuthor = document.createElement('div');
+    tileAuthor.className = ("tileAuthor");
+    bookTile.appendChild(tileAuthor);
+    tileAuthor.innerHTML = "<br>by "+ myLibrary[myLibrary.length - 1].author;
+
+    const tilePages = document.createElement('div');
+    tilePages.className = ("tilePages");
+    bookTile.appendChild(tilePages);
+    tilePages.innerHTML = "<br>"+ myLibrary[myLibrary.length - 1].pages + " pages <br><br>";
+
+
+    //bookTile.innerHTML = myLibrary[myLibrary.length - 1].title + "<br>by "+ myLibrary[myLibrary.length - 1].author + "<br>Pages: "+ myLibrary[myLibrary.length - 1].pages + "<br><br>"; 
+}
 
 const printLibrary = function() {
     for (let i=0; i < myLibrary.length; i++){
-        bookList.innerHTML += "Title: " + myLibrary[i].title + "<br>Author: "+ myLibrary[i].author + "<br>Pages: "+ myLibrary[i].pages + "<br><br>"; 
+        bookList.bookTile.innerHTML = "Title: " + '<style class="tileTitle">' + myLibrary[i].title + "<br>Author: "+ myLibrary[i].author + "<br>Pages: "+ myLibrary[i].pages + "<br><br>"; 
     }
-}
-
-const printNewBook = function(){
-    bookList.innerHTML += "Title: " + myLibrary[myLibrary.length - 1].title + "<br>Author: "+ myLibrary[myLibrary.length - 1].author + "<br>Pages: "+ myLibrary[myLibrary.length - 1].pages + "<br><br>"; 
 }
 
 window.onload = () => printLibrary();
@@ -40,7 +67,44 @@ function addBookToLibrary(book) {
     printNewBook();
 }
 
-bookButton.addEventListener('click', (book) =>  {
+submitBtn.addEventListener('click', (book) =>  {
     addBookToLibrary(book) ;
+    addBookBtn.style.visibility = "visible";
+    form.style.visibility = "hidden";
+    bookList.style.marginTop = "-6vh";
 });
+
+
+addBookBtn.addEventListener('click', () =>  {
+    form.style.visibility = "visible";
+    addBookBtn.style.visibility = "hidden";
+    bookList.style.marginTop = "0vh";
+    function changeListMargin(x) {
+        if (x.matches) { // If media query matches
+            bookList.style.marginTop = "23vh";
+        } else {
+            bookList.style.marginTop = "3vh";
+        }
+      }
+      
+      var x = window.matchMedia("(max-width: 1200px)")
+      changeListMargin(x) 
+});
+
+if(form.style.visibility == "hidden"){
+    alert('hidden');
+    bookList.style.color = "red";
+}
+
+
+function changeListMargin(x) {
+    if (x.matches && addBookBtn.style.visibility == "hidden") { // If media query matches
+        bookList.style.marginTop = "23vh";
+    } else {
+        bookList.style.marginTop = "3vh";
+    }
+  }
+  var x = window.matchMedia("(max-width: 1200px)")
+      changeListMargin(x) 
+      x.addListener(changeListMargin); 
 
